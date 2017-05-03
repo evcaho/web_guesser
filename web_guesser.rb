5fd801a182c5b51121dfee676754e7eeec6130a4
Evie Caldwell-Horne <evcaldwell@gmail.com>
2017-05-03 15:18:12 -0700

    added guess counter

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,19 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-@@number = 1 + rand(100)
+SECRET_NUMBER = 1 + rand(100)
+@@number = SECRET_NUMBER
+@@counter = 5
 
 def guess_message(guess)
 	if guess == 0
 		message = " "
 	elsif guess > @@number + 10
-		message = "way too high"
+		message = "way too high. You have #{@@counter} guesses left"
 	elsif guess < @@number - 10
-		message = "way too low"
+		message = "way too low. You have #{@@counter} guesses left"
 	elsif guess > @@number
-		message = "too high!"
+		message = "too high! You have #{@@counter} guesses left"
 	elsif guess < @@number
-		message = "too low!"
+		message = "too low! You have #{@@counter} guesses left"
 	elsif guess == @@number
 		message = "you guessed correctly! The number is #{@@number}"
 	end
@@ -29,9 +31,20 @@ def color(guess)
 	end
 end
 
+def guesses(guess)
+	if @@counter == 1
+		@@counter = 5
+		@@number = 1 + rand(100)
+		message = "You're out of guesses. Let's try a new number"
+	else
+		@@counter -= 1
+		message = guess_message(guess)
+	end
+end
+
 get '/' do
 	guess = params["guess"].to_i
-	message = guess_message(guess)
+	message = guesses(guess)
 	color = color(guess)
 	erb :index, :locals => {:number => @@number, :color => color, :message => message}
 end
