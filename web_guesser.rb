f0ec7033bf994f88d1e7241424df10ffbf0c3372
Evie Caldwell-Horne <evcaldwell@gmail.com>
2017-05-03 14:43:42 -0700

    color picker works

--- web_guesser.rb
+++ web_guesser.rb
@@ -19,9 +19,20 @@ def guess_message(guess)
 	end
 end
 
+def color(guess)
+	color = case
+	when guess == 0 then "white"
+	when guess == @@number then "green"
+	when guess < @@number - 5 || guess > @@number + 5 then "red"
+	when guess < @@number && guess >= @@number -1 then "orange"
+	when guess > @@number && guess <= @@number + 5 then "orange"
+	end
+end
+
 get '/' do
 	guess = params["guess"].to_i
 	message = guess_message(guess)
-	erb :index, :locals => {:number => @@number, :message => message}
+	color = color(guess)
+	erb :index, :locals => {:number => @@number, :color => color, :message => message}
 end
 
