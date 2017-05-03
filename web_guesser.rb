6617f3dd2cd1f08d5f95ff88a1bdeef0697b0b7d
Evie Caldwell-Horne <evcaldwell@gmail.com>
2017-05-03 13:47:01 -0700

    guesser messaging works

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-number = rand(101)
+@@number = 1 + rand(100)
 
-get '/' do 
-	erb :index, :locals => {:number => number}
+def guess_message(guess)
+	if guess == 0
+		message = " "
+	elsif guess > @@number
+		message = "too high!"
+	elsif guess < @@number
+		message = "too low!"
+	elsif guess == @@number
+		message = "you guessed correctly!"
+	end
+end
+
+get '/' do
+	guess = params["guess"].to_i
+	message = guess_message(guess)
+	erb :index, :locals => {:number => @@number, :message => message}
 end
 
